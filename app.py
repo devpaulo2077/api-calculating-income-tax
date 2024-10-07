ba1c50d7fd09a1fc2c7a2c06b3635bfe5145da81
devpaulo2077 <dev.paulorc@gmail.com>
2024-10-06 23:52:22 -0300

    delete: Deleting vercel files.

--- app.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
-
-app = get_wsgi_application()
